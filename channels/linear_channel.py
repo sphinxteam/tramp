87d874a3dca3896f000311e8bf40651d8ab2db9b
Antoine Baker <antoine.baker59@gmail.com>
2019-01-28 19:56:41 +0100

    fix 0 and infinity limit in linear channel

--- channels/linear_channel.py
+++ channels/linear_channel.py
@@ -1,5 +1,6 @@
 import numpy as np
 from ..base import Channel
+import logging
 
 
 def svd(X):
@@ -13,21 +14,24 @@ def svd(X):
 
 
 class LinearChannel(Channel):
-    def __init__(self, W, precompute_svd = True):
+    "Linear channel x = Wz"
+
+    def __init__(self, W, precompute_svd=True):
         self.Nx = W.shape[0]
         self.Nz = W.shape[1]
-        self.precompute_svd = True
+        self.precompute_svd = precompute_svd
         self.repr_init()
         self.W = W
+        self.rank = np.linalg.matrix_rank(W)
+        self.alpha = self.Nx / self.Nz
         if precompute_svd:
-            self.U, S, self.V = svd(W)
-            self.S = S
-            self.spectrum = np.diag(S.T @ S)
-            assert self.spectrum.shape == (self.Nz,)
+            self.U, self.S, self.V = svd(W)
+            self.spectrum = np.diag(self.S.T @ self.S)
         else:
             self.C = W.T @ W
             self.spectrum = np.linalg.eigvalsh(self.C)
-            assert self.spectrum.shape == (self.Nz,)
+        assert self.spectrum.shape == (self.Nz,)
+        self.singular = self.spectrum[:self.rank]
 
     def sample(self, Z):
         X = self.W @ Z
@@ -39,36 +43,71 @@ class LinearChannel(Channel):
     def second_moment(self, tau):
         return tau * self.spectrum.sum() / self.Nx
 
-    def compute_backward_posterior(self, az, bz, ax, bx):
+    def compute_n_eff(self, az, ax):
+        "Effective number of parameters = overlap in z"
+        if ax == 0:
+            logging.warn(f"ax=0 in {self} compute_n_eff")
+            return 0.
+        if az / ax == 0:
+            logging.warn(f"az/ax=0 in {self} compute_n_eff")
+            return self.rank / self.Nz
+        n_eff_trace = np.sum(self.singular / (az / ax + self.singular))
+        return n_eff_trace / self.Nz
+
+    def compute_backward_mean(self, az, bz, ax, bx):
         # estimate z from x = Wz
-        resolvent = 1 / (az + ax * self.spectrum)
         if self.precompute_svd:
             bx_svd = self.U.T @ bx
             bz_svd = self.V.T @ bz
+            resolvent = 1 / (az + ax * self.spectrum)
             rz_svd = resolvent * (bz_svd + self.S.T @ bx_svd)
             rz = self.V @ rz_svd
         else:
             a = az * np.identity(self.Nz) + ax * self.C
             b = (bz + self.W.T @ bx)
             rz = np.linalg.solve(a, b)
-        vz = resolvent.mean()
+        return rz
+
+    def compute_forward_mean(self, az, bz, ax, bx):
+        # estimate x from x = Wz we have rx = W rz
+        rz = self.compute_backward_mean(az, bz, ax, bx)
+        rx = self.W @ rz
+        return rx
+
+    def compute_backward_variance(self, az, ax):
+        if az == 0:
+            logging.warn(f"az=0 in {self} compute_backward_variance")
+            i_mean = np.mean(1 / self.singular)
+            return np.inf if self.rank < self.Nz else i_mean / ax
+        n_eff = self.compute_n_eff(az, ax)
+        vz = (1 - n_eff) / az
+        return vz
+
+    def compute_forward_variance(self, az, ax):
+        if ax == 0:
+            logging.warn(f"ax=0 in {self} compute_forward_variance")
+            s_mean = np.mean(self.singular)
+            return s_mean * self.rank / (self.Nx * az)
+        n_eff = self.compute_n_eff(az, ax)
+        vx = n_eff / (self.alpha * ax)
+        return vx
+
+    def compute_backward_posterior(self, az, bz, ax, bx):
+        # estimate z from x = Wz
+        rz = self.compute_backward_mean(az, bz, ax, bx)
+        vz = self.compute_backward_variance(az, ax)
         return rz, vz
 
     def compute_forward_posterior(self, az, bz, ax, bx):
         # estimate x from x = Wz
-        rz, vz = self.compute_backward_posterior(az, bz, ax, bx)
-        # rx = W rz and Nx ax vx + Nz az vz = Nz
-        rx = self.W @ rz
-        vx = (self.Nz / self.Nx) * (1 - az * vz) / ax
+        rx = self.compute_forward_mean(az, bz, ax, bx)
+        vx = self.compute_forward_variance(az, ax)
         return rx, vx
 
     def compute_backward_error(self, az, ax, tau):
-        resolvent = 1 / (az + ax * self.spectrum)
-        vz = resolvent.mean()
+        vz = self.compute_backward_variance(az, ax)
         return vz
 
     def compute_forward_error(self, az, ax, tau):
-        resolvent = 1 / (az + ax * self.spectrum)
-        vz = resolvent.mean()
-        vx = (self.Nz / self.Nx) * (1 - az * vz) / ax
+        vx = self.compute_forward_variance(az, ax)
         return vx
