fix numerical instability when v small

algos/message_passing.py
@@ -1,11 +1,18 @@
 import numpy as np
 import networkx as nx
 import logging
-from ..base import Variable
+from ..base import Variable, Factor
 from ..models import DAGModel
 from .initial_conditions import ConstantInit
 from .callbacks import PassCallback
 
+def find_variable_in_nodes(id, nodes):
+    matchs = [
+        node for node in nodes
+        if isinstance(node, Variable) and node.id == id
+    ]
+    assert len(matchs)==1
+    return matchs[0]
 
 class MessagePassing():
     _default_initializer = ConstantInit(a=0, b=0)
@@ -43,6 +50,17 @@ class MessagePassing():
         self.message_dag = message_dag
         nx.freeze(self.message_dag)
 
+    def reset_message_dag(self, message_dag):
+        self.message_dag = message_dag
+        self.variables = [
+            node for node in message_dag.nodes()
+            if isinstance(node, Variable)
+        ]
+        self.factors = [
+            node for node in message_dag.nodes()
+            if isinstance(node, Factor)
+        ]
+
     def update_message(self, new_message):
         for source, target, new_data in new_message:
             n_iter = self.message_dag[source][target]["n_iter"]
@@ -75,7 +93,44 @@ class MessagePassing():
             variables_data.append(data)
         return variables_data
 
-    def iterate(self, max_iter, callback=None, initializer=None, warm_start=False):
+    def check_any_small(self):
+        "Returns True if any v is below epsilon."
+        epsilon = 1e-10
+        any_small = False
+        for variable in self.variables:
+            new_v = self.message_dag.node[variable]["v"]
+            if new_v < epsilon:
+                any_small = True
+                logging.info(f"new_v={new_v}<epsilon={epsilon} for {variable}")
+        return any_small
+
+    def check_any_nan(self):
+        "Returns True if any v is nan."
+        any_nan = False
+        for variable in self.variables:
+            new_v = self.message_dag.node[variable]["v"]
+            if np.isnan(new_v):
+                any_nan = True
+                logging.info(f"new_v is nan for {variable}")
+        return any_nan
+
+    def check_any_increasing(self, old_message_dag):
+        "Returns True if any v is increasing."
+        any_increasing = False
+        for variable in self.variables:
+            old_variable = find_variable_in_nodes(
+                variable.id, old_message_dag.nodes()
+            )
+            old_v = old_message_dag.node[old_variable]["v"]
+            new_v = self.message_dag.node[variable]["v"]
+            if new_v > old_v:
+                any_increasing = True
+                logging.info(f"new_v={new_v}>old_v={old_v} for {variable}")
+        return any_increasing
+
+    def iterate(self, max_iter,
+                callback=None, initializer=None, warm_start=False,
+                check_nan=True, check_decreasing=True):
         initializer = initializer or self._default_initializer
         callback = callback or self._default_callback
         if warm_start:
@@ -87,9 +142,29 @@ class MessagePassing():
             self.n_iter = 0
             self.init_message_dag(initializer)
         for i in range(max_iter):
+            # backup message_dag
+            if (i>0) and (check_nan or check_decreasing):
+                old_message_dag = self.message_dag.copy()
+            # forward, backward, update pass
             self.forward_message()
             self.backward_message()
             self.update_variables()
+            # early stoppings with restoring message_dag
+            if (i>0) and check_nan:
+                any_nan = self.check_any_nan()
+                if any_nan:
+                    logging.warn("nan v: restoring old message dag")
+                    self.reset_message_dag(old_message_dag)
+                    logging.info(f"terminated after n_iter={self.n_iter} iterations")
+                    return
+            if (i>0) and check_decreasing:
+                any_increasing = self.check_any_increasing(old_message_dag)
+                if any_increasing:
+                    logging.warn("increasing v: restoring old message dag")
+                    self.reset_message_dag(old_message_dag)
+                    logging.info(f"terminated after n_iter={self.n_iter} iterations")
+                    return
+            # callbacks
             self.n_iter += 1
             stop = callback(self, i, max_iter)
             logging.debug(f"n_iter={self.n_iter}")

likelihoods/sng_likelihood.py
@@ -5,20 +5,22 @@ from ..base import Likelihood
 from ..utils.integration import gaussian_measure
 from scipy.integrate import quad
 import logging
-
+import warnings
 
 def phi_0(x):
-    return 0.5 * erfcx(-x / np.sqrt(2))
-
+    "Computes N(x)/Phi(x)"
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        d = np.sqrt(2 * np.pi) * 0.5 * erfcx(-x / np.sqrt(2))
+    return 1./d
 
 def psi(x):
-    phi = np.sqrt(2 * np.pi) * phi_0(x)
-    return x + 1 / phi
-
+    phi = phi_0(x)
+    return x + phi
 
 def psi_prime(x):
-    phi = np.sqrt(2 * np.pi) * phi_0(x)
-    return 1 - (1 / phi) * (x + 1 / phi)
+    phi = phi_0(x)
+    return 1 - phi * (x + phi)
 
 
 class SngLikelihood(Likelihood):