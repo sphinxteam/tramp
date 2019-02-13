a09eda0897b3c39e60aa462c11ec68a13139ee65
Antoine Baker <antoine.baker59@gmail.com>
2019-02-13 09:10:59 +0100

    add ternary channel

--- ensembles/__init__.py
+++ ensembles/__init__.py
@@ -2,13 +2,15 @@ from .gaussian_ensemble import GaussianEnsemble
 from .rotation_ensemble import RotationEnsemble
 from .unitary_ensemble import UnitaryEnsemble
 from .binary_ensemble import BinaryEnsemble
+from .ternary_ensemble import TernaryEnsemble
 
 
 ENSEMBLE_CLASSES = {
     "gaussian": GaussianEnsemble,
     "rotation": RotationEnsemble,
     "unitary": UnitaryEnsemble,
-    "binary": BinaryEnsemble
+    "binary": BinaryEnsemble,
+    "ternary": TernaryEnsemble
 }
 
 
@@ -18,5 +20,8 @@ def get_ensemble(ensemble_type, **kwargs):
         ensemble_kwargs["M"] = kwargs["M"]
     if ensemble_type == "binary":
         ensemble_kwargs["p_pos"] = kwargs["p_pos"]
+    if ensemble_type == "ternary":
+        ensemble_kwargs["p_pos"] = kwargs["p_pos"]
+        ensemble_kwargs["p_neg"] = kwargs["p_neg"]
     ensemble = ENSEMBLE_CLASSES[ensemble_type](**ensemble_kwargs)
     return ensemble
--- ensembles/ternary_ensemble.py
+++ ensembles/ternary_ensemble.py
@@ -2,27 +2,29 @@ import numpy as np
 from ..base import Ensemble
 
 
-class BinaryEnsemble(Ensemble):
-    def __init__(self, M, N, p_pos=0.5):
+class TernaryEnsemble(Ensemble):
+    def __init__(self, M, N, p_pos=0.33, p_neg=0.33):
         self.M = M
         self.N = N
-        self.p_pos
+        self.p_pos = p_pos
+        self.p_neg = p_neg
         self.repr_init()
-        self.p_neg = 1 - self.p_pos
+        self.p_zero = 1 - (self.p_pos + self.p_neg)
 
     def generate(self):
-        """Generate binary iid matrix.
+        """Generate ternary iid matrix.
 
         Returns
         -------
         - X : array of shape (M, N)
-            Binary iid X where
+            Ternary iid X where
             - X = + 1 / sqrt(N) with proba p_pos
+            - X = 0 with proba p_zero
             - X = - 1 / sqrt(N) with proba p_neg
         """
-        p = [self.p_neg, self.p_pos]
+        p = [self.p_neg, self.p_zero, self.p_pos]
         X = np.random.choice(
-            [-1, +1], size=(self.M, self.N), replace=True, p=p
+            [-1, 0, +1], size=(self.M, self.N), replace=True, p=p
         )
         sigma_x = 1 / np.sqrt(self.N)
         X *= sigma_x
