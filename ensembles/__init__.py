a89c29289e9a78473f7db9550bfe133caf3f535b
Antoine Baker <antoine.baker59@gmail.com>
2019-02-13 09:04:41 +0100

    add binary ensemble

--- ensembles/__init__.py
+++ ensembles/__init__.py
@@ -1,17 +1,22 @@
 from .gaussian_ensemble import GaussianEnsemble
 from .rotation_ensemble import RotationEnsemble
 from .unitary_ensemble import UnitaryEnsemble
+from .binary_ensemble import BinaryEnsemble
 
 
 ENSEMBLE_CLASSES = {
     "gaussian": GaussianEnsemble,
     "rotation": RotationEnsemble,
-    "unitary": UnitaryEnsemble
+    "unitary": UnitaryEnsemble,
+    "binary": BinaryEnsemble
 }
+
 
 def get_ensemble(ensemble_type, **kwargs):
     ensemble_kwargs = dict(N=kwargs["N"])
-    if ensemble_type in ["gaussian"]:
+    if ensemble_type in ["gaussian", "binary"]:
         ensemble_kwargs["M"] = kwargs["M"]
+    if ensemble_type == "binary":
+        ensemble_kwargs["p_pos"] = kwargs["p_pos"]
     ensemble = ENSEMBLE_CLASSES[ensemble_type](**ensemble_kwargs)
     return ensemble
---
+++ ensembles/binary_ensemble.py
@@ -0,0 +1,29 @@
+import numpy as np
+from ..base import Ensemble
+
+
+class BinaryEnsemble(Ensemble):
+    def __init__(self, M, N, p_pos=0.5):
+        self.M = M
+        self.N = N
+        self.p_pos
+        self.repr_init()
+        self.p_neg = 1 - self.p_pos
+
+    def generate(self):
+        """Generate binary iid matrix.
+
+        Returns
+        -------
+        - X : array of shape (M, N)
+            Binary iid X where
+            - X = + 1/ sqrt(N) with proba p_pos
+            - X = - 1/ sqrt(N) with proba p_neg
+        """
+        p = [self.p_neg, self.p_pos]
+        X = np.random.choice(
+            [-1, +1], size=(self.M, self.N), replace=True, p=p
+        )
+        sigma_x = 1 / np.sqrt(self.N)
+        X *= sigma_x
+        return X
