8a11656a8443b9a8033d8738d25bb262659bee77
Antoine Baker <antoine.baker59@gmail.com>
2019-12-10 16:52:32 +0100

    create package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='tramp',
+      version='0.1',
+      author='Antoine Baker',
+      author_email='antoine.baker59@gmail.com',
+      description='Tree approximate message passing',
+      packages=['tramp'],
+      install_requires=[
+          'numpy', 'scipy', 'pandas', 'matplotlib', 'daft',
+          'networkx>=1.10,<2'
+      ])
