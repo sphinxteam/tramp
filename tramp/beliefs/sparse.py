052265182c64ef077a950dcad4cbf2424e1982ab
Antoine Baker <antoine.baker59@gmail.com>
2021-06-03 12:02:25 +0200

    add proba sparse

--- tramp/beliefs/sparse.py
+++ tramp/beliefs/sparse.py
@@ -6,13 +6,17 @@ def A(a, b, eta):
     return np.logaddexp(eta, normal.A(a, b))
 
 
-def r(a, b, eta):
+def p(a, b, eta):
     xi = normal.A(a, b) - eta
     s = expit(xi)
+    return s
+
+
+def r(a, b, eta):
+    s = p(a, b, eta)
     return s * (b / a)
 
 
 def v(a, b, eta):
-    xi = normal.A(a, b) - eta
-    s = expit(xi)
+    s = p(a, b, eta)
     return s / a + s * (1-s) * (b / a)**2
